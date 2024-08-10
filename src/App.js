3f9decb104ac81b64df30b9f6660a781492e4da0
ahmod001 <ahmodh08@gmail.com>
2024-08-10 23:51:39 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.css";
+import Footer from "./Components/Basic/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h2 className='bg-red-600'>Hello World!</h2>
-    </div>
+    <>
+      <div className="App min-h-screen">
+        <h2 className="bg-red-600">Hello World!</h2>
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Basic/Footer.js
@@ -0,0 +1,116 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white">
+      <div className="max-w-5xl mx-auto p-4">
+        <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center py-12">
+          {/* Logo and Description */}
+          <div className="mb-8 lg:mb-0">
+            <div className="flex items-center mb-4">
+              <img src="/assets/logo.png" alt="Logo" className="" />
+            </div>
+            <p className="text-base lg:max-w-xs max-w-full">
+              Learn how to use online social media in our society and how to
+              earn online without wasting valuable time from online social
+              media.
+            </p>
+            <div className="flex mt-4 space-x-4 text-xl">
+              <a href="#" className="text-white">
+              <i class="bi bi-facebook"></i>
+              </a>
+              <a href="#" className="text-white">
+              <i class="bi bi-youtube"></i>
+              </a>
+              <a href="#" className="text-white">
+              <i class="bi bi-linkedin"></i>
+              </a>
+              <a href="#" className="text-white">
+              <i class="bi bi-telegram"></i>
+              </a>
+            </div>
+          </div>
+
+          {/* Company Links */}
+          <div className="mb-8 lg:mb-0">
+            <h4 className="font-semibold text-xl mb-5">Company</h4>
+            <ul className="space-y-3">
+              <li>
+                <a href="#" className="text-base hover:underline">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base hover:underline">
+                  FAQ
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base hover:underline">
+                  Blogs
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Support Links */}
+          <div className="mb-8 lg:mb-0">
+            <h4 className="font-semibold text-xl mb-5">Support</h4>
+            <ul className="space-y-3">
+              <li>
+                <a href="#" className="text-base hover:underline">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base hover:underline">
+                  Support
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-base hover:underline">
+                  Courses
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Contact Info */}
+          <div>
+            <h4 className="font-semibold text-xl mb-5">Contact Info</h4>
+            <ul className="space-y-3">
+              <li className="text-base flex items-center">
+              <i class="bi bi-geo-alt-fill mr-3 text-lg text-purple-700"></i>
+                Mymensingh City
+              </li>
+              <li className="text-base flex items-center">
+              <i class="bi bi-telephone-fill mr-3 text-lg text-purple-700"></i>
+                01870000000
+              </li>
+              <li className="text-base flex items-center">
+              <i class="bi bi-envelope-fill mr-3 text-lg text-purple-700"></i>
+                adviceelearningplatform@gmail.com
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="mt-8 flex flex-col lg:flex-row justify-between items-center border-t border-gray-700 pb-4 pt-8 lg:space-y-0 space-y-4">
+          {/* Payment gatways */}
+          <img src="/assets/payment-gateways.png" alt="payment gateways" />
+
+          {/* Copywrite */}
+          <p className="text-xs">
+            &copy; 2023 Advice E-learning Platform. All Rights Reserved.
+          </p>
+
+          <a href="#" className="text-xs hover:underline font-medium">
+            Become Instructor
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
