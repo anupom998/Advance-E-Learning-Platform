2a99317eee029025c9594043b136f42ab1548b7e
ahmod001 <ahmodh08@gmail.com>
2024-08-11 21:01:25 +0600

    update: footer max  width increased 5xl -> 6xl

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-black text-white">
-      <div className="max-w-5xl mx-auto p-4">
+      <div className="max-w-6xl mx-auto p-4">
         <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center py-12">
     
           <div className="mb-8 lg:mb-0">
