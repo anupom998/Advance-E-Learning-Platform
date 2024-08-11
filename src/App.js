9e278992b6d1e084e92d3a524623dbfdc01f0513
ahmod001 <ahmodh08@gmail.com>
2024-08-11 23:13:21 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
-import Footer from "./Components/Footer"
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <>
+    <main>
+      <Header />
+
       <div className="App min-h-screen">
-        <h2 className="bg-red-600">Hello World!</h2>
+        {/* <h2 className="bg-purple-light">Hello World!</h2> */}
       </div>
 
-      <Footer/>
-    </>
+      <Footer />
+    </main>
   );
 }
 
