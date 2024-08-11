7399932bf4b149e9517440523684fac9c5bb851c
ahmod001 <ahmodh08@gmail.com>
2024-08-11 20:58:33 +0600

    update: custom purpal color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "purple-dark": "#5E3FD7",
+        "purple-light": "#6F6FFF"
+      }
+    },
   },
   plugins: [],
 }
