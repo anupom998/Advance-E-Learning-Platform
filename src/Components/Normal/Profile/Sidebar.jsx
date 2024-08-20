813ca478f8244210669a43e5fc62ce4416d64c29
ahmod001 <ahmodh08@gmail.com>
2024-08-20 13:28:54 +0600

    update: sidebar student Name added

--- src/Components/Normal/Profile/Sidebar.jsx
+++ src/Components/Normal/Profile/Sidebar.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { useSearchParams } from 'react-router-dom';
-
+import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 /**
- * 
+ * @param {string} [studentName]
  * @param {Array} [links]
  * 
  * Example: [{
@@ -14,27 +13,34 @@ import { useSearchParams } from 'react-router-dom';
               title: "Change Password",
               urlParam: "change-password"
             }]
- * @returns 
  */
 
-const Sidebar = ({links}) => {
-    const [searchParams, setSearchParams] = useSearchParams();
+const Sidebar = ({ studentName, links }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    return (
-        <aside className="md:col-span-2 w-full">
+  return (
+    <aside className="md:col-span-2  w-full">
+      {/* Name */}
+      <h1 className="text-xl mb-5 font-semibold px-10">{studentName}</h1>
+      <ul className="space-y-0.5">
+        {links.map((item, i) => {
+          let isActive = searchParams.get("s") === item?.urlParam;
 
-
-        <ul>
-          {links.map((item, i) => {
-            let isActive = searchParams.get('s') === item?.urlParam;
-
-            return (<li key={i} className={`${isActive && 'bg-[#F8F7FD]'} py-3 capitalize px-10 w-full font-semibold text-gray-500 cursor-pointer select-none`} onClick={() => setSearchParams({ s: item?.urlParam })}>
+          return (
+            <li
+              key={i}
+              className={`${
+                isActive && "bg-[#F8F7FD]"
+              } py-3 capitalize px-10 w-full font-semibold text-gray-500 cursor-pointer select-none text-base`}
+              onClick={() => setSearchParams({ s: item?.urlParam })}
+            >
               {item?.title}
-            </li>)
-          })}
-        </ul>
-      </aside>
-    );
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
