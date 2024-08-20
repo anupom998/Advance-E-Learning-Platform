d2fb54393e44a36fd48063db6dbf7e815a12d2a7
ahmod001 <ahmodh08@gmail.com>
2024-08-20 15:36:20 +0600

    update: textfiled updated

---
+++ src/Components/Library/Form/Textarea.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+/**
+ *
+ * @param {string} [title]
+ * @param {string} [className] used for external classes [optional]
+ * @param {any} [...props]
+ * @param {string} [placeholder]
+ */
+
+const Textarea = ({ title, className, placeholder, type, ...props }) => {
+  return (
+    <div>
+      <label
+        htmlFor="first-name"
+        className="block text-sm font-semibold mb-2.5 capitalize"
+      >
+        {title}
+      </label>
+      <textarea
+        {...props}
+        name={title}
+        placeholder={placeholder ? placeholder : title}
+        className={`w-full p-3 border border-gray-300 rounded-sm  placeholder-gray-400  ${className}`}
+      ></textarea>
+    </div>
+  );
+};
+
+export default Textarea;
