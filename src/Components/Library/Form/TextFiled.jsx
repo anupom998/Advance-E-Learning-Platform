1b97e0d2cb4f9eea0e7e7102c4069831da5202ee
ahmod001 <ahmodh08@gmail.com>
2024-08-20 15:36:48 +0600

    make: textarea created

--- src/Components/Library/Form/TextFiled.jsx
+++ src/Components/Library/Form/TextFiled.jsx
@@ -6,22 +6,22 @@ import React from "react";
  * @param {string} [className] used for external classes [optional]
  * @param {string} [type] used for setting input filed type [optional]
  * @param {any} [...props]
- * @returns
+ * @param {string} [placeholder]
  */
 
-const TextFiled = ({ title, className, type, ...props }) => {
+const TextFiled = ({ title, className, placeholder, type, ...props }) => {
   return (
-    <div className="mb-4">
+    <div>
       <label
         htmlFor="first-name"
-        className="block text-sm font-semibold mb-3 capitalize"
+        className="block text-sm font-semibold mb-2.5 capitalize"
       >
         {title}
       </label>
       <input
         {...props}
         name={title}
-        placeholder={title}
+        placeholder={placeholder ? placeholder : title}
         type={type ? type : "text"} //Default type is Text
         className={`w-full p-3 border border-gray-300 rounded-sm  placeholder-gray-400  ${className}`}
       />
