3ed06e47b2e492fc8dba30a74d383e1e83c1c0f8
ahmod001 <ahmodh08@gmail.com>
2024-08-20 12:16:22 +0600

    Update: Student/ProfileSetting page migrated to the Normal/Profile

--- src/Pages/Normal/Profile.js
+++ src/Pages/Normal/Profile.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import BreadCrumb from "../../Components/Library/BreadCrumb";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <main className="min-h-screen  py-20 bg-gray-50">
+      <header className="text-center">
+        {/* Name */}
+        <h1 className="text-3xl md:text-4xl mb-5 font-semibold">Hey, {"Ahmod Hasan"} 👋</h1>
 
-export default Profile
+        <BreadCrumb
+          links={[
+            {
+              pageName: "Home",
+              route: "/",
+            },
+            {
+              pageName: "Profile",
+              route: "/student/profile",
+              lock: true,
+            },
+          ]}
+        />
+      </header>
+      <section className=" space-y-10"></section>
+    </main>
+  );
+};
+
+export default Profile;
