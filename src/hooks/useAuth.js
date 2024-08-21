df8b230954fdfee8f52d4b60d5a2b0a8c7ec89cc
ahmod001 <ahmodh08@gmail.com>
2024-08-22 00:04:12 +0600

    update: Layout glitch fixced

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const useAuth = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(!false); // Default to not logged in
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Default to not logged in
 
   const login = () => setIsLoggedIn(true);   // Function to log in
   const logout = () => setIsLoggedIn(false); // Function to log out
