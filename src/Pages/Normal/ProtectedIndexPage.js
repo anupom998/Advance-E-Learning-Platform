59dd5eaa6f6c12a4ae5bbb66907ee1f144d8c697
ahmod001 <ahmodh08@gmail.com>
2024-08-26 19:57:49 +0600

    update: imge issue fixed

--- src/Pages/Normal/ProtectedIndexPage.js
+++ src/Pages/Normal/ProtectedIndexPage.js
@@ -8,17 +8,17 @@ const ProtectedIndexPage = () => {
   return (
     <main className="min-h-screen">
       {/* Hero Section */}
-     <div className="bg-purple-light flex items-center justify-center">
-  <div className="max-w-6xl w-full mx-auto px-4 pt-14 pb-20 text-white flex justify-center md:justify-end">
-    <img src={OfficeImg} alt="Office" />
-  </div>
-</div>
+      <div className="bg-purple-light flex items-center justify-center">
+        <div className="max-w-6xl w-full mx-auto px-4 py-10 text-white flex justify-center md:justify-end">
+          <img src={OfficeImg} className="md:max-h-[30rem] max-h-[20rem]" alt="Office" />
+        </div>
+      </div>
 
       {/* Middle Section */}
       <MiddleSection />
       <section className="bg-gray-50 pb-20 space-y-10">
         <CourseBenefits />
-        <AboutUs/>
+        <AboutUs />
       </section>
     </main>
   );
