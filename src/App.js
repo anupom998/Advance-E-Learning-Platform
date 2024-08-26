128f3bc4c19462ebca4fcbc158ee3610398931c2
ahmod001 <ahmodh08@gmail.com>
2024-08-26 19:57:36 +0600

    update: useAuth path correctted

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 // layouts
 import HomeLayout from "./Layouts/HomeLayout";
 import StudentLayout from "./Layouts/StudentLayout";
 
 // logins
 import SignUp from "./Pages/Login/SignUp";
-import SignIn from "./Pages/Login/SignIn"
-import AdminLogIn from "./Pages/Login/AdminLogIn"
-import SubAdminLogIn from "./Pages/Login/SubAdminLogIn"
+import SignIn from "./Pages/Login/SignIn";
+import AdminLogIn from "./Pages/Login/AdminLogIn";
+import SubAdminLogIn from "./Pages/Login/SubAdminLogIn";
 
 // main pages
 import HomeWork from "./Pages/Normal/HomeWork";
 import AirdropEarn from "./Pages/Normal/AirdropEarn";
-import MyLearning from "./Pages/Normal/MyLearning"
-import Profile from "./Pages/Normal/Profile"
-import VideoEarn from "./Pages/Normal/VideoEarn"
-import useAuth from "./hooks/useAuth";
+import MyLearning from "./Pages/Normal/MyLearning";
+import Profile from "./Pages/Normal/Profile";
+import VideoEarn from "./Pages/Normal/VideoEarn";
+import useAuth from "./Hooks/useAuth";
 import IndexPage from "./Pages/IndexPage";
 import ProtectedIndexPage from "./Pages/Normal/ProtectedIndexPage";
 import MiddleSection from "./Components/ProtectedIndexPage/MiddleSection";
-
-// const App = () => {
-//   return (
-//     <div>
-//       <MiddleSection />
-//     </div>
-//   )
-// }
-
 
 const App = () => {
   const { isLoggedIn } = useAuth();
@@ -39,100 +35,72 @@ const App = () => {
         {/* Public Routes - HomeLayout */}
         {!isLoggedIn ? (
           <>
-          <Route
-            path="/"
-            element={
-              <HomeLayout>
-                <IndexPage />
-              </HomeLayout>
-            }
-          />
-          <Route
-            path="/sign-up"
-            element={
-              
-                <SignUp />
-             
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              
-                <SignIn />
-              
-            }
-          />
-          <Route
-            path="/admin-login"
-            element={
-              
-                <AdminLogIn />
-              
-            }
-          />
-          <Route
-            path="/subadmin-login"
-            element={
-              
-                <SubAdminLogIn />
-              
-            }
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </>
+            <Route
+              path="/"
+              element={
+                <HomeLayout>
+                  <IndexPage />
+                </HomeLayout>
+              }
+            />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/admin-login" element={<AdminLogIn />} />
+            <Route path="/subadmin-login" element={<SubAdminLogIn />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </>
         ) : (
           // Protected Routes - StudentLayout
           <>
-          <Route
-            path="/"
-            element={
-              <StudentLayout>
-                <ProtectedIndexPage />
-              </StudentLayout>
-            }
-          />
-          <Route
-            path="/student/course"
-            element={
-              <StudentLayout>
-                <MyLearning />
-              </StudentLayout>
-            }
-          />
-          <Route
-            path="/student/my-homeworks"
-            element={
-              <StudentLayout>
-                <HomeWork />
-              </StudentLayout>
-            }
-          />
-          <Route
-            path="/student/my-earnings"
-            element={
-              <StudentLayout>
-                <VideoEarn />
-              </StudentLayout>
-            }
-          />
-          <Route
-            path="/student/airdrop-earn"
-            element={
-              <StudentLayout>
-                <AirdropEarn />
-              </StudentLayout>
-            }
-          />
-          <Route
-            path="/student/profile"
-            element={
-              <StudentLayout>
-                <Profile/>
-              </StudentLayout>
-            }
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
+            <Route
+              path="/"
+              element={
+                <StudentLayout>
+                  <ProtectedIndexPage />
+                </StudentLayout>
+              }
+            />
+            <Route
+              path="/student/course"
+              element={
+                <StudentLayout>
+                  <MyLearning />
+                </StudentLayout>
+              }
+            />
+            <Route
+              path="/student/my-homeworks"
+              element={
+                <StudentLayout>
+                  <HomeWork />
+                </StudentLayout>
+              }
+            />
+            <Route
+              path="/student/my-earnings"
+              element={
+                <StudentLayout>
+                  <VideoEarn />
+                </StudentLayout>
+              }
+            />
+            <Route
+              path="/student/airdrop-earn"
+              element={
+                <StudentLayout>
+                  <AirdropEarn />
+                </StudentLayout>
+              }
+            />
+            <Route
+              path="/student/profile"
+              element={
+                <StudentLayout>
+                  <Profile />
+                </StudentLayout>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </>
         )}
       </Routes>
